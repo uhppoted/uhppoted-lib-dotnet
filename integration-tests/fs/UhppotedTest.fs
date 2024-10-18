8a4499319ae9a31be559ffefe23d6d70e5ed9e88
twystd <twystd@users.noreply.github.com>
2024-10-18 12:48:45 -0700

    Setting up integration tests

--- integration-tests/fs/UhppotedTest.fs
+++ integration-tests/fs/UhppotedTest.fs
@@ -6,6 +6,24 @@ open stub
 
 [<TestFixture>]
 type TestClass() =
+    [<OneTimeSetUp>]
+    member this.Initialise() = 
+        TestContext.Error.WriteLine("=========>OneTimeSetUp")
+
+    [<OneTimeTearDown>]
+    member this.Terminate() = 
+        TestContext.Error.WriteLine("=========>OneTimeTearDown")
+
+    [<SetUp>]
+    member this.Setup() = 
+        printfn ">>>> setup"
+
+    [<TearDown>]
+    member this.TearDown() = 
+        printfn ">>>> teardown"
 
     [<Test>]
     member this.TestEvenSequence() = Assert.That(true, Is.True)
+
+    [<Test>]
+    member this.TestEvenSequence2() = Assert.That(true, Is.True)
