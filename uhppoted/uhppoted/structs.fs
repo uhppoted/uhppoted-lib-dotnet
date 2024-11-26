c1ac73eb15fa9963c46acb69ada241fc3fd175e7
twystd <twystd@users.noreply.github.com>
2024-11-26 11:18:37 -0800

    Defined enums for door mode and event direction

--- uhppoted/uhppoted/structs.fs
+++ uhppoted/uhppoted/structs.fs
@@ -65,6 +65,15 @@ type Listener =
     { endpoint: IPEndPoint
       interval: uint8 }
 
+type DoorMode =
+    | NormallyOpen = 1
+    | NormallyClosed = 2
+    | Controlled = 3
+
+type Direction =
+    | Unknown = 0
+    | In = 1
+    | Out = 2
 
 type internal IResponse =
     abstract member controller: uint32
