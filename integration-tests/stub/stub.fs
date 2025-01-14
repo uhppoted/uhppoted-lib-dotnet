59e3aa7883e46ce5398576acc3173dec1fdfcf8a
twystd <twystd@users.noreply.github.com>
2025-01-14 11:34:49 -0800

    Reworked integration tests to use CancellationToken

--- integration-tests/stub/stub.fs
+++ integration-tests/stub/stub.fs
@@ -8,8 +8,8 @@ open System.Threading
 open System.Threading.Tasks
 
 type Emulator =
-    { udp: Option<(UdpClient * Task)>
-      tcp: Option<(TcpListener * Task)> }
+    { udp: Option<(UdpClient * Task * CancellationTokenSource)>
+      tcp: Option<(TcpListener * Task * CancellationTokenSource)> }
 
 module Stub =
     let dump packet (logger: TextWriter) =
@@ -27,48 +27,55 @@ module Stub =
         | Some(responses) -> Some(responses)
         | _ -> None
 
-    let rec recv (socket: UdpClient) (logger: TextWriter) =
+    let rec recv (socket: UdpClient) (cancel: CancellationToken) (logger: TextWriter) =
         async {
             try
-                let! (packet, remote) =
-                    Async.FromBeginEnd(
-                        (fun (callback, state) -> socket.BeginReceive(callback, state)),
-                        (fun (iar) ->
-                            let addr = ref Unchecked.defaultof<IPEndPoint>
-                            let packet = socket.EndReceive(iar, addr)
-                            (packet, !addr))
-                    )
+                let! result = socket.ReceiveAsync(cancel).AsTask() |> Async.AwaitTask
 
-                match find packet with
-                | Some(replies) ->
-                    for reply in replies do
-                        socket.Send(reply, reply.Length, remote) |> ignore
-                | _ ->
-                    logger.WriteLine("*** ERROR unknown packet")
-                    dump packet logger
+                match result.Buffer.Length with
+                | 64 ->
+                    let packet = result.Buffer.[0..63]
+                    let remote = result.RemoteEndPoint
 
-                return! recv socket logger
+                    match find packet with
+                    | Some(replies) ->
+                        for reply in replies do
+                            socket.Send(reply, reply.Length, remote) |> ignore
+                    | _ ->
+                        logger.WriteLine("*** ERROR unknown packet")
+                        dump packet logger
+
+                | N -> logger.WriteLine($"*** WARN invalid packet size {N}")
+
+                return! recv socket cancel logger
             with
-            | :? ObjectDisposedException -> ()
-            | err -> logger.WriteLine("** ERROR/1 {0}", err)
+            | :? OperationCanceledException -> socket.Close()
+
+            | :? ObjectDisposedException -> socket.Close()
+
+            | :? SocketException as x ->
+                logger.WriteLine($"** ERROR udp::recv socket exception: {x.Message}")
+                socket.Close()
+
+            | err ->
+                logger.WriteLine($"** ERROR udp::recv {err.Message}")
+                socket.Close()
         }
 
-    let rec read (stream: NetworkStream) (logger: TextWriter) =
+    let rec read (stream: NetworkStream) (cancel: CancellationToken) (logger: TextWriter) =
         async {
             try
-                let! packet =
-                    let buffer = Array.zeroCreate 1024
+                let buffer = Array.zeroCreate<byte> 1024
+                let! N = stream.ReadAsync(buffer, 0, buffer.Length, cancel) |> Async.AwaitTask
 
-                    Async.FromBeginEnd(
-                        (fun (callback, state) -> stream.BeginRead(buffer, 0, buffer.Length, callback, state)),
-                        (fun (iar) ->
-                            let N = stream.EndRead(iar)
-                            buffer[0 .. N - 1])
-                    )
-
-                if packet.Length <= 0 then
+                if N <= 0 then
+                    stream.Close()
                     return ()
+                else if N <> 64 then
+                    logger.WriteLine($"*** WARN invalid packet size {N}")
                 else
+                    let packet = buffer.[0..63]
+
                     match find packet with
                     | Some(replies) ->
                         for reply in replies do
@@ -77,61 +84,84 @@ module Stub =
                         logger.WriteLine("*** ERROR unknown packet")
                         dump packet logger
 
-                    return! read stream logger
-            with err ->
-                logger.WriteLine("*** ERROR/2 {0}", Error err)
+                    return! read stream cancel logger
+            with
+            | :? OperationCanceledException ->
+                logger.WriteLine("** INFO TCP receive operation canceled.")
+                stream.Close()
 
-            return ()
+            | :? IOException as x ->
+                logger.WriteLine($"** ERROR tcp::recv IO exception: {x.Message}")
+                stream.Close()
+
+            | :? SocketException as x ->
+                logger.WriteLine($"** ERROR tcp::recv socket exception: {x.Message}")
+                stream.Close()
+
+            | err ->
+                logger.WriteLine($"** ERROR tcp::recv {err.Message}")
+                stream.Close()
         }
 
-    let listen (socket: TcpListener) (logger: TextWriter) =
+    let listen (socket: TcpListener) (cancel: CancellationToken) (logger: TextWriter) =
         async {
             try
                 socket.Start()
 
                 while true do
-                    let! connection = socket.AcceptTcpClientAsync() |> Async.AwaitTask
+                    let! connection = (socket.AcceptTcpClientAsync(cancel).AsTask()) |> Async.AwaitTask
                     let stream = connection.GetStream()
 
-                    read stream logger |> Async.Start
+                    read stream cancel logger |> Async.Start
 
             with
-            | :? ObjectDisposedException -> ()
-            | :? AggregateException as x ->
-                match x.InnerException with
-                | :? SocketException as xx when xx.SocketErrorCode = SocketError.OperationAborted -> ()
-                | _ -> logger.WriteLine("** ERROR/3 {0}", x.Message)
-            | err -> logger.WriteLine("** ERROR/4 {0}", err.Message)
+            | :? OperationCanceledException -> socket.Server.Close()
+            | :? ObjectDisposedException -> socket.Server.Close()
+            | :? SocketException as x when x.SocketErrorCode = SocketError.OperationAborted ->
+                logger.WriteLine("** INFO tcp::accept operation aborted.")
+                socket.Server.Close()
+            | err ->
+                logger.WriteLine($"** ERROR tcp::accept {err.Message}")
+                socket.Server.Close()
         }
 
     let init (mode: string) (logger: TextWriter) : Result<Emulator, string> =
         match mode with
         | "broadcast" ->
+            let cts = new CancellationTokenSource()
             let udp = new UdpClient(39999)
 
             udp.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true)
 
-            let udprx = recv udp logger |> Async.StartAsTask
+            let udprx = recv udp cts.Token logger |> Async.StartAsTask
 
-            Ok { udp = Some(udp, udprx); tcp = None }
+            Ok
+                { udp = Some(udp, udprx, cts)
+                  tcp = None }
 
         | "connected udp" ->
+            let cts = new CancellationTokenSource()
             let udp = new UdpClient(39998)
 
             udp.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true)
 
-            let udprx = recv udp logger |> Async.StartAsTask
+            let udprx = recv udp cts.Token logger |> Async.StartAsTask
 
-            Ok { udp = Some(udp, udprx); tcp = None }
+            Ok
+                { udp = Some(udp, udprx, cts)
+                  tcp = None }
 
         | "tcp" ->
+            let cts = new CancellationTokenSource()
             let tcp = new TcpListener(IPAddress.Any, 39997)
 
             tcp.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true)
 
-            let tcprx = listen tcp logger |> Async.StartAsTask
+            let tcprx = listen tcp cts.Token logger |> Async.StartAsTask
 
-            Ok { udp = None; tcp = Some(tcp, tcprx) }
+            Ok
+                { udp = None
+                  tcp = Some(tcp, tcprx, cts) }
 
         | _ ->
             logger.WriteLine("unknown emulator mode ({0})", mode)
@@ -154,13 +184,22 @@ module Stub =
 
     let terminate (emulator: Emulator) (logger: TextWriter) =
         match emulator.udp with
-        | Some udp -> (fst udp).Close()
+        | Some(udp, udprx, cts) ->
+            cts.Cancel()
+
+            if not (udprx.Wait(5000)) then
+                logger.WriteLine("  ** WARN udp::recv cancel timeout")
+                udp.Close()
         | None -> ()
 
         match emulator.tcp with
-        | Some tcp ->
-            (fst tcp).Server.Close()
-            // (fst tcp).Stop()
+        | Some(tcp, tcprx, cts) ->
+            cts.Cancel()
+
+            if not (tcprx.Wait(5000)) then
+                logger.WriteLine("  ** WARN tcp::read cancel timeout")
+                tcp.Server.Close()
+
             Thread.Sleep(5000)
         | None -> ()
 
